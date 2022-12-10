06bf19ba12c69f90ed018aff7e82164f0e31b704
thekillerregs <thekillerregs@gmail.com>
2022-12-10 19:08:13 -0300

    Custom ServerPing

--- src/main/java/thekillerregs/tkbungee/TkBungee.java
+++ src/main/java/thekillerregs/tkbungee/TkBungee.java
@@ -2,22 +2,41 @@ package thekillerregs.tkbungee;
 
 
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.Favicon;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
 
 
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
-public final class TkBungee extends Plugin  {
+public final class TkBungee extends Plugin implements Listener  {
+
+    private Favicon favicon;
 
     @Override
     public void onEnable() {
-    getProxy().getPluginManager().registerCommand(this, new FruitCommand());
+
+        try{
+            favicon = Favicon.create(ImageIO.read(new File("favicon.png")));
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+
+    getProxy().getPluginManager().registerListener(this, this);
 
 
 
@@ -29,6 +48,17 @@ public final class TkBungee extends Plugin  {
     }
 
 
+    @EventHandler
+    public void onProxyPing(ProxyPingEvent e)
+    {
+        ServerPing ping = e.getResponse();
+        ping.setDescription(ChatColor.AQUA+" vose jogaria agui\n vc teria corage");
+        ping.setPlayers(new ServerPing.Players(30000, 16789, ping.getPlayers().getSample()));
+        ping.setVersion(new ServerPing.Protocol("Usa 1.17.1 nessa bomba taok", 756));
+        ping.setFavicon(favicon);
+
+        e.setResponse(ping);
+    }
 
 
 }
